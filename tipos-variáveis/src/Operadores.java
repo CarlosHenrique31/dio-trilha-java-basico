267bfd1e4a24e1706158b92b88e14c3dce834e25
Carlos Henrique <carlos.pigmentar@gmail.com>
2025-03-09 20:43:45 -0400

    operadores

---
+++ tipos-variáveis/src/Operadores.java
@@ -0,0 +1,131 @@
+public class Operadores {
+    public static void main(String[] args){
+
+/* 
+classe Operadores.java
+
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+qual o resultado das expressoes abaixo?
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+
+unarios 
+
+      int numero = 5;
+      
+      numero = - numero;      
+
+      System.out.println(numero);
+
+      numero = numero * -1;
+
+      System.out.println(numero);
+
+repetição
+
+        int numero = 5;
+
+        numero = numero + 2;
+
+        System.out.println(numero);
+
+incremento
+
+        System.out.println( ++ numero);
+
+        System.out.println(numero);
+
+decremento
+
+        System.out.println( numero -- );
+
+        System.out.println(numero);
+
+negação
+
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+ternarios
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = "";
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+relacionais
+
+        String nomeUm = "GLEYSON";
+        String nomeDois = new String("GLEYSON");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente de numeroDois? " + simNao);
+    
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois? " + simNao);
+*/    
+//lógicos
+
+         boolean condicao = true;
+
+         boolean condicao2 = false;
+
+         if(condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+         }
+
+         if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+         }
+
+         System.out.println("fim");
+
+    }
+}
